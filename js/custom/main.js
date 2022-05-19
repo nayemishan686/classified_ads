560cd3ad32e5daf6ab0e4228b0655e98bc89ad52
Nayem Ishan <nayemishan377@gmail.com>
2022-05-20 00:04:51 +0600

    testimonial section added

--- js/custom/main.js
+++ js/custom/main.js
@@ -76,3 +76,30 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+//code for testimonial
+const clientTabItems = document.querySelectorAll('.client-item');
+const showDiv = document.querySelector('.show-info');
+
+clientTabItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        showInfo(item);
+    });
+});
+
+function showInfo(item) {
+    showDiv.querySelector('.show-img img').src = item.querySelector('.client-thumbnail img').src;
+    showDiv.querySelector('.show-name').innerHTML = item.querySelector('.client-name').innerHTML;
+    showDiv.querySelector('.show-designation').innerHTML = item.querySelector('.client-designation').innerHTML;
+    showDiv.querySelector('.show-description').innerHTML = item.querySelector('.client-description').innerHTML;
+    setActiveTab(item);
+}
+
+function setActiveTab(item) {
+    clientTabItems.forEach((item) => {
+        item.classList.remove('active'); // resetting active tab
+    });
+    item.classList.add('active');
+}
+
+showInfo(clientTabItems[0]); // default active tab
